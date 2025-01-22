b65e692d4398dc3ed1135be5498bd5cc44ac5d91
clnmcdowell <114112169+clnmcdowell@users.noreply.github.com>
2025-01-22 13:52:10 -0500

    Outer loop and variable initialization

--- geo_match.py
+++ geo_match.py
@@ -1,2 +1,8 @@
-def geo_match(array1, array2):
+from haversine import haversine, Unit
+
+def geo_match(array1, array2): 
     matches = []
+
+    for location1 in array1: # Loop through each point in array 1
+        closest_loc = None 
+        min_dist = float('inf') 
